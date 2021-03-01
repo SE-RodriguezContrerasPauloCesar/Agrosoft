7ef1f737ec29a9ced9a60eff7f92f9719644137d
Paulo Cesar Rodriguez Contreras <prodriguezc@unsa.edu.pe>
2021-03-01 17:25:22 -0500

    Actualización de repositorio a otra cuenta

--- agrosoft_accounts/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AgrosoftAccountsConfig(AppConfig):
-    name = 'agrosoft_accounts'
--- agrosoft_web/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AgrosoftWebConfig(AppConfig):
-    name = 'agrosoft_web'
